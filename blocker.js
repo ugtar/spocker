ff89ecd7b57dd406087fc8c1a9cb978be7ad9e52
Uri Okrent <uokrent@gmail.com>
2012-12-03 14:08:22 +0200

    initial blocking works on facebook for hard-coded string
    Signed-off-by: Uri Okrent <uokrent@gmail.com>

---
+++ blocker.js
@@ -0,0 +1,32 @@
+function insert_shim(elem) {
+	var parentNode = elem.parentNode;
+	var shim = document.createElement('div');
+	shim.className = "blocker-shim";
+	shim.innerText = "Click to show spoiler!";
+	shim.onclick = function() {
+		shim.style.display = "none";
+		elem.style.display = "block";
+	};
+	parentNode.insertBefore(shim, elem);
+}
+
+function hide_spoilers() {
+	var potential_spoilers = document.getElementsByClassName("userContent");
+	for (var pt in potential_spoilers) {
+		var offending = /.*walking dead.*/i;
+		elem = potential_spoilers[pt];
+		if (elem.previousSibling && elem.previousSibling.className == "blocker-shim")
+			continue;
+		if (offending.test(elem.innerText)) {
+			insert_shim(elem);
+			elem.style.display = "none";
+		}
+	}
+}
+
+var stream_elem = document.getElementById("home_stream");
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(hide_spoilers);
+var config = {childList: true};
+observer.observe(stream_elem, config);
+hide_spoilers();
